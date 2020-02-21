c738091fb457b6ee6adb9e8f4fcd4697b7eb7ffd
Nicolas <nicolasgoldman07@gmail.com>
2020-02-21 19:21:15 -0300

    minor changes on doubleprod

---
+++ dobleProductor-consumidor-full/Debug/example/src/freertos_blinky.d
@@ -0,0 +1,2 @@
+example/src/freertos_blinky.o example/src/freertos_blinky.d: \
+ ../example/src/freertos_blinky.c
---
+++ dobleProductor-consumidor-full/Debug/example/src/subdir.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../example/src/PP-C-doble.c \
+../example/src/cr_startup_lpc175x_6x.c \
+../example/src/sysinit.c \
+../example/src/trcKernelPort.c \
+../example/src/trcSnapshotRecorder.c \
+../example/src/trcStreamingRecorder.c 
+
+OBJS += \
+./example/src/PP-C-doble.o \
+./example/src/cr_startup_lpc175x_6x.o \
+./example/src/sysinit.o \
+./example/src/trcKernelPort.o \
+./example/src/trcSnapshotRecorder.o \
+./example/src/trcStreamingRecorder.o 
+
+C_DEPS += \
+./example/src/PP-C-doble.d \
+./example/src/cr_startup_lpc175x_6x.d \
+./example/src/sysinit.d \
+./example/src/trcKernelPort.d \
+./example/src/trcSnapshotRecorder.d \
+./example/src/trcStreamingRecorder.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+example/src/%.o: ../example/src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DDEBUG -D__CODE_RED -D__USE_LPCOPEN -DCORE_M3 -DSDK_DEBUGCONSOLE=1 -I"/home/nico/Documents/MCUXpresso_11.1.0_3209/workspace/lpcopen_2_10_lpcxpresso_nxp_lpcxpresso_1769.zip_expanded/lpc_chip_175x_6x/inc" -I"/home/nico/Documents/MCUXpresso_11.1.0_3209/workspace/lpcopen_2_10_lpcxpresso_nxp_lpcxpresso_1769.zip_expanded/lpc_board_nxp_lpcxpresso_1769/inc" -I"/home/nico/Documents/MCUXpresso_11.1.0_3209/workspace/dobleProductor-consumidor-full/example/inc" -I"/home/nico/Documents/MCUXpresso_11.1.0_3209/workspace/dobleProductor-consumidor-full/freertos/inc" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fmerge-constants -fmacro-prefix-map="../$(@D)/"=. -mcpu=cortex-m3 -mthumb -fstack-usage -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
